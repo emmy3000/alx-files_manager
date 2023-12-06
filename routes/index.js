e3bb51245efeb438e06ac2ae09a42f81f68f2ce4
Emeka Emodi <emodiemeka@gmail.com>
2023-12-06 21:53:53 +0100

    feat: Create 'routes/index.js' defining API endpoints: GET /status and GET /stats

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,20 @@
 /**
  * @file routes/index.js
  * @description Defines all endpoints for the API.
- * Contains routes such as GET /status and GET /stats.
+ * Contains routes such as GET /status, GET /stats, and POST /users.
  */
 
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
+// Existing routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// New user creation route
+router.post('/users', UsersController.postNew);
 
 export default router;
