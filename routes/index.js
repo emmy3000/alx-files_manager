5d81b4ff60715384d6b6831bb9611ca6e1edbc76
Emeka Emodi <emodiemeka@gmail.com>
2023-12-06 22:16:28 +0100

    Add new endpoint for POST /files in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,12 @@
 
 import express from 'express';
 import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
-import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
 export default router;
