29b5bfab1206ffc9acaf0c1ac7f25985f0831772
Emeka Emodi <emodiemeka@gmail.com>
2023-12-06 22:21:11 +0100

    Add endpoints for showing and listing files in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,11 @@
  */
 
 import express from 'express';
-import AppController from '../controllers/AppController';
 import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 export default router;
