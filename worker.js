d756f6dca48b5fc4753e710def93eef3fb656b9d
Emeka Emodi <emodiemeka@gmail.com>
2023-12-06 23:18:59 +0100

    feat: implement background processing for sending welcome email to new users

--- worker.js
+++ worker.js
@@ -4,44 +4,29 @@
 // Processes the queue, generating 3 thumbnails with widths of 500, 250, and 100 pixels.
 // Updates the endpoint GET /files/:id/data to accept a query parameter 'size' for serving different thumbnail sizes.
 
-import bull from 'bull';
+import Bull from 'bull';
 import dbClient from './utils/db';
-import thumbnail from 'image-thumbnail';
 
-const fileQueue = new bull('fileQueue');
+// Create a queue userQueue
+const userQueue = new Bull('userQueue');
 
-fileQueue.process(async (job) => {
-  try {
-    const { userId, fileId } = job.data;
+// Process the userQueue
+userQueue.process(async job => {
+  const { userId } = job.data;
 
-    if (!fileId) {
-      throw new Error('Missing fileId');
-    }
-
-    if (!userId) {
-      throw new Error('Missing userId');
-    }
-
-    const file = await dbClient.getFileById(userId, fileId);
-
-    if (!file) {
-      throw new Error('File not found');
-    }
-
-    const imagePath = file.localPath;
-    const imageBuffer = fs.readFileSync(imagePath);
-
-    const sizes = [500, 250, 100];
-    sizes.forEach((size) => {
-      const thumbnailPath = `/tmp/files_manager/${fileId}_${size}`;
-      thumbnail(imageBuffer, { width: size, height: size }).then((thumbnailBuffer) => {
-        fs.writeFileSync(thumbnailPath, thumbnailBuffer);
-      });
-    });
-
-    return true;
-  } catch (error) {
-    console.error('Error processing fileQueue job:', error.message);
-    throw error;
+  // If userId is not present in the job, raise an error Missing userId
+  if (!userId) {
+    throw new Error('Missing userId');
   }
+
+  // If no document is found in DB based on the userId, raise an error User not found
+  const user = await dbClient.getUserById(userId);
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  // Print in the console Welcome <email>!
+  console.log(`Welcome ${user.email}!`);
 });
+
+export default userQueue;
