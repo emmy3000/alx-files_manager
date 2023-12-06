c1ef3b4dab6b45d0523bb67caaa7875d6fc03bcc
Emeka Emodi <emodiemeka@gmail.com>
2023-12-06 22:06:34 +0100

    Add routes for status and stats endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@
 /**
  * @file routes/index.js
  * @description Defines all endpoints for the API.
- * Contains routes such as GET /status, GET /stats, and POST /users.
+ * Contains routes such as GET /status, GET /stats, GET /connect, GET /disconnect, and GET /users/me.
  */
 
 import express from 'express';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
-// Existing routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// New user creation route
-router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
